191b9bacd1c89437774205aadfa2d530ff811c35
Gabriel Dautreppe <gabriel.dautreppe@gmail.com>
2023-04-17 21:44:39 +0200

    ✨ add js

---
+++ script.js
@@ -0,0 +1,21 @@
+const modelPath = 'path/to/model.pv';
+const keywordPathLight = 'path/to/light.ppn';
+const keywordPathOff = 'path/to/off.ppn';
+const sensitivity = 0.5;
+
+const voiceDetectorLight = pv_VoiceDetector.create(modelPath, keywordPathLight, sensitivity);
+voiceDetectorLight.addCallback((keyword) => {
+    if (keyword === 'light') {
+        document.getElementById('lamp-img').src = 'image2.jpg';
+    }
+});
+
+const voiceDetectorOff = pv_VoiceDetector.create(modelPath, keywordPathOff, sensitivity);
+voiceDetectorOff.addCallback((keyword) => {
+    if (keyword === 'off') {
+        document.getElementById('lamp-img').src = 'image1.jpg';
+    }
+});
+
+voiceDetectorLight.start();
+voiceDetectorOff.start();
